9394c45bd946e048565625d815dad8ccecc58523
AbeMonty <abraham.montoya1990@gmail.com>
2021-02-06 15:51:22 -0600

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 
 const router = require('./router/router');
-const { databaseConnect } = require('./database/databaseConnect');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+//const { databaseConnect } = require('./database/databaseConnect');
 
 const app = express();
 
@@ -19,7 +22,29 @@ app.get('/', (req, res)=>{
 app.use('/api', router);
 
 // Connect to Database
-databaseConnect();
+//databaseConnect();
+
+const dbURL = process.env.DATABASE_URL;
+
+
+const databaseConnect = async ()=>{
+    try {
+
+        const connect = await mongoose.connect( dbURL , {
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useNewUrlParser: true
+        });
+
+        console.log(`DB Connected. ${connect.connection.name}`);
+
+    } catch (error) {
+        console.log(`Error in connecting to DataBase: ${error.message}`)
+    }
+}
+
+databaseConnect()
+
 
 const PORT = process.env.PORT || 5000;
 
